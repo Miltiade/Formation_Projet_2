9cb41e67b921a57c3f169d8c856f9f928f3b6487
Miltiade <114372308+Miltiade@users.noreply.github.com>
2023-04-20 18:36:43 +0300

    Finished code for scraping "books category"

--- extract_category_data.py
+++ extract_category_data.py
@@ -2,33 +2,39 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
+
 url = "http://books.toscrape.com/catalogue/category/books/nonfiction_13/index.html"
+
+
+books = []
+
 response = requests.get(url)
 
 soup = BeautifulSoup(response.content, 'html.parser')
-books = soup.find_all('article', {'class': 'product_pod'})
+books += soup.find_all('article', {'class': 'product_pod'})
+while soup.find('li', {'class': 'next'}):
+    next_url = soup.find('li', {'class': 'next'}).find('a').get('href')
+    url = "/".join(url.split('/')[:-1]) + '/' + next_url
+    response = requests.get(url)
 
-# I. Récupérer toutes les URL des liens depuis la page Catégorie
-Books_URL = soup.find_all('a')
-for link in Books_URL:
-    print(link.get('href'))
+    soup = BeautifulSoup(response.content, 'html.parser')
+    books += soup.find_all('article', {'class': 'product_pod'})
 
 # II. Encapsuler le script de l'étape 1 dans une fonction / Passer l'URL à mon code qui parse les pages Livre
 
 # Définir la fonction
-def scrape_data(books):
+def scrape_data(product_page_url):
     ##a) Récupération de "product_page_url"
-    product_page_url = books
+    response = requests.get(product_page_url)
+    book = BeautifulSoup(response.content, 'html.parser')
     ##b) Récupération de "universal_ product_code (upc)"
-    universal_product_code = (
-        books.find("table", class_="table table-striped").find("td").text
-    )
+    universal_product_code = book.find("table", class_="table table-striped").find("td").text
     ##c) Récupération de "title"
-    div = books.find("div", class_="product_main")
+    div = book.find("div", class_="product_main")
     title = div.find("h1").text
     ##d) Récupération... des données qui se trouvent dans la même zone de la page web (i.e. "table")
     # Définition du périmètre de ladite zone
-    table = books.find("table", class_="table table-striped")
+    table = book.find("table", class_="table table-striped")
     table_td = table.find_all("td")
     # Récupération de price_including_tax
     price_including_tax = table_td[3].text
@@ -38,27 +44,15 @@ def scrape_data(books):
     number_available = table_td[5].text
     ##e) Récupération des données qui se trouvent ailleurs dans la page
     # Récupération de product_description
-    product_description = books.find_all("p")[3].text
+    product_description = book.find_all("p")[3].text
     # Récupération de category
-    category = books.find_all("a")[3].text
+    category = book.find_all("a")[3].text
     # Récupération de review_rating
     review_rating = div.find("p", class_="star-rating").attrs["class"][1]
     # Récupération de image_url
-    image_url = books.find("img").attrs["src"]
+    image_url = book.find("img").attrs["src"]
     # Définir, comme variables, la ligne dite "header" et la ligne dite "données"
-    header = [
-        "product_page_url",
-        "universal_product_code",
-        "title",
-        "price_including_tax",
-        "price_excluding_tax",
-        "number_available",
-        "product_description",
-        "category",
-        "review_rating",
-        "image_url",
-        ]
-    values = [
+    return [
         product_page_url,
         universal_product_code,
         title,
@@ -69,14 +63,32 @@ def scrape_data(books):
         category,
         review_rating,
         image_url,
-        ]
-    return
-    with open("data_produit.csv","w") as file:
-        csv_writer = csv.writer(file)
-        csv_writer.writerow(header)
-        csv_writer.writerow(values)
+    ]
+    
 
 # Appeler la fonction
-print(scrape_data)
+header = [
+    "product_page_url",
+    "universal_product_code",
+    "title",
+    "price_including_tax",
+    "price_excluding_tax",
+    "number_available",
+    "product_description",
+    "category",
+    "review_rating",
+    "image_url",
+    ]
+values = []
+for link in books:
+    url = link.find('a').get("href")
+    url = "http://books.toscrape.com/catalogue/" + url[9:]
+    values.append(scrape_data(url))
+
+
+with open("data_produit.csv","w") as file:
+    csv_writer = csv.writer(file)
+    csv_writer.writerow(header)
+    csv_writer.writerows(values)
 
 print("Data has been scraped and written to 'data_produit.csv' file.")

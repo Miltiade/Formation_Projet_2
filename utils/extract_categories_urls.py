fc980d30d9ae5c1a9a6de9218402ed600128febe
Miltiade <114372308+Miltiade@users.noreply.github.com>
2023-04-26 12:31:22 +0300

    created module "extract categories_urls"

---
+++ utils/extract_categories_urls.py
@@ -0,0 +1,25 @@
+# From the website's homepage (https://books.toscrape.com/) : extract urls of all books categories
+
+# Import relevant libraries
+import requests
+from bs4 import BeautifulSoup
+
+# Define URL
+url="http://books.toscrape.com/index.html"
+
+# Extract every book category's url
+def extract_categories_url(url):
+    # Declare empty list
+    categories_urls = []
+    # Create Soup object that will be parsed
+    response = requests.get(url)
+    # Parse the page
+    soup = BeautifulSoup(response.content, 'html.parser')
+    categories_urls = soup.find_all({'class': 'nav nav-list'})
+    for link in categories_urls:
+        url = link.find('a').get("href")
+        url = "http://books.toscrape.com/" + url[4:]
+        categories_urls.append(url)
+    return categories_urls
+
+    

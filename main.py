394f1ad3de996b4e93e2f3545aeb957b14242dfe
Miltiade <114372308+Miltiade@users.noreply.github.com>
2023-04-26 12:31:44 +0300

    adapted to "extract_cat_urls" modularization

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 # Import relevant libraries
 from utils.csv import generate as generate_csv
 from utils.category import scrape_category
+from utils.extract_categories_urls import extract_categories_url
 
-# From homepage :
-# Extract all category pages url
+# Define URL of website to scrape
+url = "http://books.toscrape.com/index.html"
 
-# For loop code below on url below
+# From the website's homepage : extract urls of all books categories
+extract_categories_url(url)
 
-# Define URL
-url = "http://books.toscrape.com/catalogue/category/books/nonfiction_13/index.html"
+# Create a "for" loop code on url below
+for url in extract_categories_url(url):
+    scrape_category(url)
 
 # Extract category data for url
 values= scrape_category(url)
@@ -26,9 +29,11 @@ header = [
     "review_rating",
     "image_url",
     ]
-
 # Variabilize category name using module url
 generate_csv(header, values, "nonfiction.csv")
-# download images based on image_url in values
 
-print("Data has been scraped and written to csv file.")
+# download images based on image_url in values
+from utils.image import download
+download(values)
+
+print("Data has been scraped and written to csv files.")

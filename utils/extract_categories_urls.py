2b80b27c6fa830c36d189bafbd611c44c8e53035
Miltiade <114372308+Miltiade@users.noreply.github.com>
2023-05-18 09:09:27 +0300

    debugged :')

--- utils/extract_categories_urls.py
+++ utils/extract_categories_urls.py
@@ -1,24 +1,37 @@
-## A script to extract urls of all books categories from the website's homepage (https://books.toscrape.com/)
+'''A script to extract URLs of all books categories from the website's homepage (https://books.toscrape.com/)'''
 
 # Import relevant libraries
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
-# Define URL
-url="http://books.toscrape.com/index.html"
+url = "http://books.toscrape.com/index.html"
 
 # Extract every book category's url
 def extract_categories_url(url):
     # Declare empty list
-    # categories_urls = []
+    categories_urls = []
     # Create Soup object that will be parsed
     response = requests.get(url)
     # Parse the page
     soup = BeautifulSoup(response.content, 'html.parser')
-    categories_urls = soup.find_all({'class': 'nav nav-list'})
-    print(categories_urls)
-    for link in categories_urls:
-        url = link.find('a').get("href")
-        # url = "http://books.toscrape.com/" + url[4:]
-        # categories_urls.append(url)
+
+    sidebar = soup.find('div', class_='side_categories')  # Find the sidebar element
+
+    category_links = sidebar.find_all('a')  # Find all the category links
+
+    for link in category_links:
+        category_url = urljoin(url, link['href'])  # Construct the full URL using urljoin
+        categories_urls.append(category_url)
+
     return categories_urls
+
+category_urls = extract_categories_url(url)
+print(category_urls)
+
+# extract_categories_url(url)
+
+# TESTING THE FUNCTION ON A SPECIFIC URL:
+# url = "http://books.toscrape.com/index.html"
+# result = extract_categories_url(url)
+# print(result)

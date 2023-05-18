a9398288ff301fb859de42f31d884abf49c4baaa
Miltiade <114372308+Miltiade@users.noreply.github.com>
2023-05-18 08:14:22 +0300

    Debugged :')

--- utils/category.py
+++ utils/category.py
@@ -1,10 +1,10 @@
-# A function that scrapes all the books's URLs from a category page
+# A function that scrapes, from a Books category page, all the books' requested data
 
 import requests
 from bs4 import BeautifulSoup
 from product import scrape as scrape_product
 
-def scrape_category(url):
+def scrape_category(url):  
     # Declare empty list
     books = []
     # Create Soup object that will be parsed
@@ -14,7 +14,6 @@ def scrape_category(url):
     books += soup.find_all('article', {'class': 'product_pod'})
     # Parse the next pages
     while soup.find('li', {'class': 'next'}):
-        print(len(books))
         next_url = soup.find('li', {'class': 'next'}).find('a').get('href')
         url = "/".join(url.split('/')[:-1]) + '/' + next_url
         response = requests.get(url)
@@ -25,5 +24,9 @@ def scrape_category(url):
         url = link.find('a').get("href")
         url = "http://books.toscrape.com/catalogue/" + url[9:]
         values.append(scrape_product(url))
-    
     return values
+
+# # TESTING THE FUNCTION ON A SPECIFIC URL:
+# url = "https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html"
+# result = scrape_category(url)
+# print(result)

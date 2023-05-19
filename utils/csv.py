fd2aa6cf7febe2a1180417e4c5c981707527a857
Miltiade <114372308+Miltiade@users.noreply.github.com>
2023-05-19 16:23:23 +0300

    renamed file

--- utils/csv.py
+++
@@ -1,9 +0,0 @@
-'''A script that generates 1 CSV file with scrapped data as values'''
-
-from csv import writer
-
-def generate(header,values,filename="data.csv"):
-    with open(filename,"w") as file:
-        csv_writer = writer(file)
-        csv_writer.writerow(header)
-        csv_writer.writerows(values)

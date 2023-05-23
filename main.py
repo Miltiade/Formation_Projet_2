a0ea19fd4af587d2aa5d9b0dcbb503d96fadbdaa
Miltiade <114372308+Miltiade@users.noreply.github.com>
2023-05-23 17:04:58 +0300

    debugged

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ from utils.scrape_category import scrape_category
 # from utils.extract_categories_urls import extract_categories_url
 # from utils.url import extract_category_name
 from utils.create_output_files_directory import create_output_files_directory
-from utils.create_category_directories import create_category_directories
-from utils.csv_write_save import generate_and_save
+# from utils.create_category_directories import create_category_directories
 
 # For ONE category: scrape all books data and generate CSV file
 
 #1. Define as variable the URL to scrape
-cat_url = "https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html"
+cat_url = "https://books.toscrape.com/catalogue/category/books/music_14/index.html"
 #2. Scrape products data from this URL
 result = scrape_category(cat_url)
 
@@ -22,20 +21,6 @@ create_output_files_directory()
 # create_category_directories()
 
 #4.a. Generate CSV file with scraped product data; create adequate category directories; save it there
-header = [
-        "product_page_url",
-        "universal_product_code",
-        "title",
-        "price_including_tax",
-        "price_excluding_tax",
-        "number_available",
-        "product_description",
-        "category",
-        "review_rating",
-        "image_url",
-        ]
-values = [result]
-for book_data in values:
-    generate_and_save(header, book_data)
+generate_and_save(result)
 print("file generated and saved successfully! Evohe! Onnea!")
 #5. For every book in the category: save image in "category_sequential_art" directory

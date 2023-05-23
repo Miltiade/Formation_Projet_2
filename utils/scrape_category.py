6d9b7b14c5f0a999c072de09453be4dfcfd0f418
Miltiade <114372308+Miltiade@users.noreply.github.com>
2023-05-23 16:44:54 +0300

    function transferred to "scrape_category"

--- utils/scrape_category.py
+++ utils/scrape_category.py
@@ -64,3 +64,16 @@ def generate_and_save(values,filename="data.csv"):
         csv_writer.writerow(HEADER)
         csv_writer.writerows(values)
     return ("file created successfully!")
+
+'''A script to download and save image file of each product'''
+
+import requests
+from .url import extract_image_name_from_url
+def download(url):
+    response = requests.get(url)
+    if response.ok:
+        name = extract_image_name_from_url(url)
+        with open("images/" + name, "wb") as file:
+            file.write(response.content)
+    else:
+        print("Erreur lors du téléchargement de l'image")

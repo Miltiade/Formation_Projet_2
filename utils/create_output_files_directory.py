6ff285823b8e94e3997f0d6e4633bdeec2df2d4b
Miltiade <114372308+Miltiade@users.noreply.github.com>
2023-05-30 18:29:27 +0300

    updated code

--- utils/create_output_files_directory.py
+++ utils/create_output_files_directory.py
@@ -2,10 +2,12 @@
 
 from pathlib import Path
 
+
 def create_output_files_directory():
     base_directory = Path("output_files")
     base_directory.mkdir(parents=True, exist_ok=True)
-    return ("directory created successfully!")
+    return "directory created successfully!"
+
 
 # TEST DE LA FONCTION DEFINIE CI-DESSUS:
 # result = create_output_files_directory()
